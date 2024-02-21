e2cad6cc1236445479fb282d337478d8aed7f55d
RaupeCHR <raupechr@gmail.com>
2024-02-21 12:10:53 +0100

    fix

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const http = require('http');
-const fs = require('fs');
-
-http.createServer(function (req, res) {
-    fs.readFile('index.html', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+console.log("hello world whats going on here")
