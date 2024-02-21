3c0d370cb00e5e2c1bf6fefe5185e4f58a7274b5
RaupeCHR <raupechr@gmail.com>
2024-02-21 12:00:15 +0100

    extend

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world whats going on here")
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+    fs.readFile('index.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
